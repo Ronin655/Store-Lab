25038ddf5b2d6ecddd04fc5ede22a2df18681fe1
Abyss Walker <lastxronin@gmail.com>
2021-09-11 03:20:24 +0300

    Добавил api для индикаторов; Tasks;

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\HelloController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', [HelloController::class, 'hello']);
