4a1b5c38c0c235f2f9a3d94ab9dbbf4a9ba167ef
uvays.aleroev <uvays.aleroev@x5.ru>
2022-01-10 17:14:01 +0300

    feat: add order model

---
+++ database/migrations/2022_01_08_151128_create_orders_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->float('price');
+            $table->smallInteger('status');
+            $table->foreignId('user_id')->constrained();
+            $table->timestamps();
+        });
+
+        Schema::create('order_items', function (Blueprint $table){
+            $table->id();
+            $table->integer('quantity');
+            $table->foreignId('order_id')->constrained();
+            $table->foreignId('product_id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_items');
+        Schema::dropIfExists('orders');
+    }
+}
